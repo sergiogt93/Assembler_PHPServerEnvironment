4b12b7e8ad2e92f3750c4599a297335e5b12902f
sergiogt93 <sergio.ber0556@gmail.com>
2021-12-15 12:43:13 +0100

    Add some design css

--- index.php
+++ index.php
@@ -11,18 +11,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="validate.php" method="POST">
-        <label>
-            User: <input type="text" name="username">
-        </label>
-        <label>
-            Password:<input type="text" name="password">
-        </label>
-        <label>
-            <input type="submit" value="Send Data">
-        </label>
-    </form>
+    <div class="boxLogin">
+        <form action="validate.php" method="POST">
+            <label>
+                User: <input type="text" name="username">
+            </label>
+            <label>
+                Password:<input type="text" name="password">
+            </label>
+            <label>
+                <input type="submit" value="Send Data">
+            </label>
+        </form>
+    </div>
 </body>
 </html>
